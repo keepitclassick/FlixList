67b5e1af47369d4b7806c329ec5801ef05e8084e
keepitclassick <keepitclassick@gmail.com>
2022-02-08 16:03:47 -0500

    add star icons to movie

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,13 @@
+import { FaStar } from "react-icons/fa";
+
 const Rating = () => {
-  return <h1>Rating</h1>;
+  return (
+    <h1>
+      {[...Array(5)].map((star) => {
+        return <FaStar />;
+      })}
+    </h1>
+  );
 };
 
 export default Rating;
