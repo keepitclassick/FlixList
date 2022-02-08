af3000f6985bca083a9dfeaec1e0deae99467165
keepitclassick <keepitclassick@gmail.com>
2022-02-08 16:09:57 -0500

    add value to each star

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,11 +3,17 @@ import { FaStar } from "react-icons/fa";
 const Rating = () => {
   return (
     <h1>
-      {[...Array(5)].map((star) => {
+      {[...Array(5)].map((star, i) => {
+        const ratingVal = i + 1;
         return (
           <label>
             {" "}
-            <input type="radio" name="rating" />
+            <input
+              type="radio"
+              name="rating"
+              value={ratingVal}
+              onClick={() => setRating(ratingVal)}
+            />
             <FaStar />
           </label>
         );
